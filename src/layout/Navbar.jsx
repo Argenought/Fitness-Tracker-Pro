9fe3f7dcbe9339149fc4f0684146dbc122edf6b4
Pedro Ortiz <pedro383ortiz@yahoo.com>
2026-02-12 10:45:20 -0800

    Something went wronf with these changes and will push the branch to reset my main branch

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,28 @@
+import { NavLink } from "react-router";
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
+        <NavLink to="/activities">Activities</NavLink>
+        {token ? (
+          <a onClick={() => logout()}>Log out</a>
+        ) : (
+          <>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+{
+  /* <a onClick={() => setPage("activities")}>Activities</a>
         {token ? (
           <a onClick={() => logout()}>Log out</a>
         ) : (
@@ -17,8 +30,5 @@ export default function Navbar() {
             <a onClick={() => setPage("register")}>Register</a>
             <a onClick={() => setPage("login")}>Login</a>
           </>
-        )}
-      </nav>
-    </header>
-  );
+        )} */
 }
